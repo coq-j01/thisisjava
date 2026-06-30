8e4d57748c3eff05fbcf8a3b56ea21337bf85918
coq-j01 <129778338+coq-j01@users.noreply.github.com>
2026-06-30 16:40:18 +0900

    static 내부에서 멤버변수 호출 불가 내용 추가

--- src/ch06/sec10/exam01/Calculator.java
+++ src/ch06/sec10/exam01/Calculator.java
@@ -1,21 +1,24 @@
 package ch06.sec10.exam01;
 
 public class Calculator {
-//	static double pi = 3.14159;
-	double pi = 3.14159;
+	static double pi = 3.14159;
+//	double pi = 3.14159;
+	int a=10; //멤버변수
 
-//	static int plus(int x, int y) {
-//		return x + y;
-//	}
-//	
-//	static int minus(int x, int y) {
-//		return x - y;
-//	}
-	int plus(int x, int y) {
+	static int plus(int x, int y) {
+//		this.pi = 20.0; // this 호출 불가
+//		a = 20; // 멤버변수 호출 불가
 		return x + y;
 	}
 	
-	int minus(int x, int y) {
+	static int minus(int x, int y) {
 		return x - y;
 	}
+//	int plus(int x, int y) {
+//		return x + y;
+//	}
+//	
+//	int minus(int x, int y) {
+//		return x - y;
+//	}
 }
