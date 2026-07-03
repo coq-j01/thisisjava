5618a3a821c83747f04968698bb0e66c4da26cba
coq-j01 <129778338+coq-j01@users.noreply.github.com>
2026-07-03 16:20:34 +0900

    10강 모듈, 라이브러리 코드 실습

---
+++ src/kosa/PackTest.java
@@ -0,0 +1,14 @@
+package kosa;
+
+import pack1.A;
+import pack2.B;
+
+public class PackTest {
+
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.method();
+        
+        new B().method();
+    }
+}
---
+++ src/pack1/A.java
@@ -0,0 +1,7 @@
+package pack1;
+
+public class A {
+	public void method() {
+		System.out.println("A 클래스 method() 호출");
+	}
+}
